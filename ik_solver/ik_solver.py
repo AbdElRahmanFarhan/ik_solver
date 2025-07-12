55741cc5b4c51992decedeeff582930afabeb2ef
AbdElRahmanFarhan <abdofarhan75@gmail.com>
2025-07-12 20:16:02 +0000

    ik working on fewer examples

--- ik_solver/ik_solver.py
+++ ik_solver/ik_solver.py
@@ -4,7 +4,7 @@ from urdf_parser_py import urdf
 from typing import List, Tuple
 from pathlib import Path
 from scipy.spatial.transform import Rotation as R
-from utils import get_tf_fixed_joint, get_tf_from_dh
+from utils import get_tf_fixed_joint, get_tf_from_dh, solve_for_delta_linear
 import numpy as np
 
 class IKSolver:
@@ -14,100 +14,99 @@ class IKSolver:
             joint for joint in self._robot.joints if joint != "fixed"]
         self.dof = len(self.active_joints)
         self.dh_parameters = {
-            "alpha": [0, -np.pi/2, 0., -np.pi/2, np.pi/2, -np.pi/2, 0],
-            "a": [0., 0.320, 1.280, 0.200, 0., 0., 0.],
-            "d": [0.78, 0., 0., 1.5925, 0., 0., 0.20],
-            "q": [0., -np.pi/2, 0., 0., 0., 0., np.pi]
+            "alpha": [0, -np.pi/2, -np.pi/2., 0., -np.pi/2, np.pi/2, -np.pi/2],
+            "a": [0., 0., 0.320, 1.280, 0.200, 0., 0.],
+            "d": [0., 0.78, 0., 0., 1.5925, 0., 0.],
+            "q": [-np.pi/2, -np.pi/2, -np.pi/2, 0., 0., 0., 0.]
         }
-        self.T_W_F = np.eye(4, 4) # Wrist in the DH to Flange
-        self.T_W_F[:3, :3] = R.from_euler("XYZ", [self.dh_parameters["alpha"][3], 0, self.dh_parameters["q"][6]]).as_matrix()
-        self.T_W_F[2, 3] = self.dh_parameters["d"][6]
+        self.T_7_EE = np.eye(4, 4) # wrist as per DH to Flange
+        self.T_7_EE[:3, :3] = R.from_euler("XYZ", [0., 0., np.pi]).as_matrix()
+        self.T_7_EE[2, 3] = 0.2
+
+        self.T_B_G = np.eye(4, 4) # base as per DH to Flange
+        self.T_B_G[:3, :3] = R.from_euler("XYZ", [np.pi, np.pi/2, 0.]).as_matrix()
+        self.T_B_G[0, 3] = 0.6
+        self.T_B_G[1, 3] = 0.43
+        self.T_B_G[2, 3] = 0.55
 
         
-    # def _gantry_FK(self) -> Matrix:
-    #     q0 = Symbol('q0')
-    #     prismatic_joint = self._robot.joints[0]
-    #     T_B_L1 = get_tf_fixed_joint(prismatic_joint)
-    #     displacement = Matrix([q0 * a for a in prismatic_joint.axis])
-    #     T_B_L1[:3, 3] += displacement
-
-    #     flange_joint = self._robot.joints[1]
-    #     T_L1_F = get_tf_fixed_joint(flange_joint)
-    #     T_B_F = T_B_L1 @ T_L1_F
-    #     return T_B_F
-    
-
-    # def forward_kinematics(self, q) -> List[float]:
-    #     q_symbols = symbols('q0:8')
-    #     subs = {q_symbol: qi for q_symbol, qi in zip(q_symbols, q)}
-    #     print(subs)
-    #     T0_EE = self.robot_FK.subs(subs)
-    #     T_B_F = self.gantry_FK.subs(subs)
-    #     T_F_0 = get_tf_fixed_joint(self._robot.joints[2])
-    #     TB_EE = T_B_F @ T_F_0 @ T0_EE
-    #     position = TB_EE[:3, 3]
-    #     quat = R.from_matrix(TB_EE[:3, :3]).as_quat()
-    #     return position, quat
-
-    # def forward_kinematics(self, q) -> List[float]:
-    #     subs = {q_symbol: qi for q_symbol, qi in zip(q_symbols, q)}
-    #     T0_EE = self.robot_FK.subs(subs)
-    #     position = list(T0_EE[:3, 3])
-    #     quat = R.from_matrix(T0_EE[:3, :3]).as_quat()
-    #     pose = [*position, *quat]
-    #     return pose    
-    
-    def forward_kinematics(self, qs):
+    def forward_kinematics(self, thetas):
         T = np.eye(4, 4)
-        for i, q in enumerate(qs):
+        for i, theta in enumerate(thetas):
             alpha = self.dh_parameters["alpha"][i]
             a = self.dh_parameters["a"][i]
-            d = self.dh_parameters["d"][i]
-            q = q + self.dh_parameters["q"][i]
+            if i == 0:
+                d = theta + self.dh_parameters["d"][i]
+                q = self.dh_parameters["q"][i]
+            else: 
+                d = self.dh_parameters["d"][i]
+                q = theta + self.dh_parameters["q"][i]
             T =  T @ get_tf_from_dh(alpha, a, d, q)
         return T
     
-    def get_pose(self, qs):
-        qs.append(0) # for the transormation to the flange
-        T = self.forward_kinematics(qs)
-        position = T[:3, 3]
-        quat = R.from_matrix(T[:3, :3]).as_quat()
+    def get_pose(self, thetas):
+        T_G_7 = self.forward_kinematics(thetas)
+        T_B_EE = self.T_B_G @ T_G_7 @ self.T_7_EE
+        position = T_B_EE[:3, 3]
+        quat = R.from_matrix(T_B_EE[:3, :3]).as_quat()
         pose = [*position, *quat]
         return pose
 
     
-    def inverse_kinematics(self, pose: List[float]) -> Tuple[float]:
+    def inverse_kinematics(self, pose: List[float], init_thetas) -> Tuple[float]:
 
-        position = pose[:3]
-        quat = pose[3:]
+        T_B_EE = np.eye(4)
+        T_B_EE [:3, 3] = pose[:3]
+        T_B_EE [:3, :3] = R.from_quat(pose[3:]).as_matrix()
 
-        R_0_EE = R.from_quat(quat).as_matrix()
-        
-        wrist_center = position - self.dh_parameters["d"][6] * R_0_EE[:, 2]
-        theta1 = np.arctan2(wrist_center[1], wrist_center[0])
+        T_G_EE = np.eye(4)
+        T_G_EE = np.linalg.inv(self.T_B_G) @ T_B_EE
+ 
+        x = T_G_EE[0, 3]
+        y = T_G_EE[1, 3]
+        limits = (0.0, 5.0)
+        robot_reach = (1.0, 5.0)
+        theta_init = init_thetas[0]
+
+        delta_theta1 = solve_for_delta_linear(x, y, limits, robot_reach, theta_init)
+
+        theta1 = delta_theta1 + T_G_EE[2, 3]
+
+
+        # after displacement
+        T_G_0 = np.eye(4)
+        T_G_0[2, 3] = theta1
+        T_0_7 = np.linalg.inv(T_G_0) @ np.linalg.inv(self.T_B_G) @ T_B_EE @ np.linalg.inv(self.T_7_EE)
+
+        X = T_0_7[0, 3]
+        Y = T_0_7[1, 3]
+        Z = T_0_7[2, 3]
+
+        theta2 = np.arctan2(-Y, Z)
             
-        side_a = np.sqrt(self.dh_parameters["a"][3]**2 + self.dh_parameters["d"][3]**2)
-        proj_z = wrist_center[2] - self.dh_parameters["d"][0]
-        proj_xy = np.sqrt(wrist_center[0]**2 + wrist_center[1]**2) - self.dh_parameters["a"][1]
+        side_a = np.sqrt(self.dh_parameters["a"][4]**2 + self.dh_parameters["d"][4]**2)
+        proj_z = X - self.dh_parameters["d"][1]
+        proj_xy = np.sqrt(Z**2 + T_0_7[1, 3]**2) - self.dh_parameters["a"][2]
         side_b = np.sqrt(proj_z**2 + proj_xy**2)
-        side_c = self.dh_parameters["a"][2]
+        side_c = self.dh_parameters["a"][3]
         angle_a = np.arccos((side_b * side_b + side_c * side_c - side_a * side_a)/(2 * side_b * side_c))
         angle_b = np.arccos((side_a * side_a + side_c * side_c - side_b * side_b)/(2 * side_a * side_c))
 
-        theta2 = np.pi/2. - angle_a - np.arctan2(proj_z, proj_xy)
-        theta3 = np.pi - angle_b - np.arctan2(self.dh_parameters["d"][3], self.dh_parameters["a"][3])
+        theta3 = np.pi/2. - angle_a - np.arctan2(proj_z, proj_xy)
+        theta4 = np.pi - angle_b - np.arctan2(self.dh_parameters["d"][4], self.dh_parameters["a"][4])
 
         
-        T_0_W = self.forward_kinematics([theta1, theta2, theta3])
-        R_0_W = T_0_W[:3, :3]
-         
-        R_W_EE = np.linalg.inv(R_0_W) @ R_0_EE @ np.linalg.inv(self.T_flange[:3, :3])
+        T_G_4 = self.forward_kinematics([theta1, theta2, theta3, theta4])
+        R_G_4 = T_G_4[:3, :3]
+        
+        R_Corr = R.from_euler("XYZ", [-np.pi/2, 0, 0]).as_matrix()
+        R_4_7 = np.linalg.inv(R_G_4) @ np.linalg.inv(self.T_B_G[:3, :3]) @ T_B_EE[:3, :3] @ np.linalg.inv(self.T_7_EE[:3, :3]) @ np.linalg.inv(R_Corr)
+        theta5, theta6, theta7 = R.from_matrix(R_4_7).as_euler("YZY")
 
-        theta4, theta5, theta6 = R.from_matrix(R_W_EE).as_euler("YZY")
-
-        thetas = [theta1, theta2, theta3, theta4, theta5, theta6]
+        thetas = [theta1, theta2, theta3, theta4, theta5, theta6, theta7]
 
         return thetas
+
 
 
 
@@ -117,12 +116,17 @@ def main():
 
     ik_solver = IKSolver(urdf_file)
 
-    pose = [0.627, 3.032, 1.894, 0.149, 0.761, 0.436, 0.456]
+    # pose = [5.386, 0.430, 2.811, 0, 0.707, 0., 0.707]
+    pose = [2.956, 0.430, 4.297, 0.000, -0.125, 0.000, 0.992]
+    init_thetas = [0, 0, 0, 0, 0, 0, 0]
 
+    thetas = ik_solver.inverse_kinematics(pose, init_thetas)
 
-    qs = ik_solver.inverse_kinematics(pose)
-    pose_solution = ik_solver.get_pose(qs)
+    print(thetas)
 
+    pose_solution = ik_solver.get_pose(thetas)
+
+    print(pose_solution)
     pose = np.array(pose).astype(float)
     pose_solution_np = np.array(pose_solution).astype(float)
 
--- ik_solver/utils.py
+++ ik_solver/utils.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from urdf_parser_py import urdf
 import numpy as np
 from scipy.spatial.transform import Rotation as R
+from scipy.optimize import minimize
 
 
 def get_tf_from_dh(alpha, a, d, q):
@@ -19,3 +20,21 @@ def get_tf_fixed_joint(joint: urdf.Joint):
     TF[:3, 3] = joint.origin.xyz
     TF[-1, -1] = 1
     return TF
+
+def solve_for_delta_linear(x, y, limits, robot_reach, theta_init):
+    objective = lambda vars: (vars[0] - theta_init)**2 
+    constraint_eq = lambda vars: vars[0] - np.sqrt(vars[1]**2 - x**2 - y**2)
+    constraint_ineq_real = lambda vars: vars[1]**2 - x**2 - y**2
+
+    bounds = [limits, robot_reach]
+    z_r_init = [theta_init, np.mean(robot_reach)]
+    constraints = [
+        {'type': 'eq',  'fun': constraint_eq},
+        {'type': 'ineq', 'fun': constraint_ineq_real},  # realness
+    ]
+
+    result = minimize(objective, z_r_init, method='SLSQP',
+                    bounds=bounds, constraints=constraints)
+    
+    z_sol, _ = result.x
+    return z_sol
